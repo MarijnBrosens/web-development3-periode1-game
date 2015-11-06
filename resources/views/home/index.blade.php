1e0f6b0e129d2eaf759612ba0e70cea0420bd099
MarijnBrosens <marijnbrosens16@hotmail.com>
2015-11-06 20:44:45 +0100

    update style homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,79 +4,40 @@
 
 @section('content')
 
-    <h1>ONE PLUS smah your old phone</h1>
+    @include('partials.home-header')
 
-    <h1>LARACAST vanaf 14 verder kijken</h1>
-
-
-
-    @if(Auth::check())
+    <main>
 
         @if($period)
 
-            <h1>{{$period->title}} ends <time datetime="{{$period->end_date}}" class="date-h"></time></h1>
+            @if(count($photos))
 
-            @include('partials/upload')
+                <div id="photos">
 
-        @else
+                    <h1>{{$period->title}}</h1>
 
-            <h3>No active period available</h3>
-            @if($nextPeriod)
+                    @include('partials/photos')
 
-                <h3>{{$nextPeriod->title}} wil start <time datetime="{{$nextPeriod->start_date}}" class="date-h"></time></h3>
+                </div>
 
             @endif
 
         @endif
 
-    @else
+        @if($pastPeriod)
 
-        <a class="btn" href="/register">Play</a>
+            <h1>Past periods</h1>
 
-    @endif
+            @foreach($pastPeriod as $period)
 
+                <h1>{{$period->title}}</h1>
+                <p>started: {{$period->start_date}}</p>
+                <p>ends: {{$period->end_date}}</p>
 
-    <h1>validatie in controller van request</h1>
-
-    <ul>
-        <li>Uitleg over wedstrijd</li>
-        <li>Prijzen</li>
-        <li>Wie er gewonnen is in een periode</li>
-        <li>Call to action</li>
-        <li>Lijst met de winnaars van de vorige periodes (automatisch gegenereerd)</li>
-        <li>Als je deelneemt, moet je NAW + email gegevens ingeven.</li>
-        <li>Hou het IP adres van de deelnemers bij</li>
-        <li>Best practices in zaken formulier validatie / security etc...</li>
-        <li>Een simpele backend waar je de lijst met de deelnemers kan raadplegen met al hun gegevens en waar je mensen kan diskwalificeren/verwijderen. (soft delete)</li>
-        <li>Personaliseer je front-end door tweaks toe te voegen om het geheel eigenheid te geven. Probeer de branding van je gekozen merk wat te integreren</li>
-        <li>een winnaar selecteren en op de homepage plaatsen, hiervan wordt ook een mail gestuurd naar de wedstrijd verantwoordelijke.</li>
-        <li>betere user check</li>
-        <li>betere period check -> als er geen periode is</li>
-    </ul>
-
-    @if($period)
-
-        @if(count($photos))
-
-            <div id="photos">
-                @include('partials/photos')
-            </div>
+            @endforeach
 
         @endif
 
-    @endif
+    </main>
 
-    @if($pastPeriod)
-
-        <h1>Past periods</h1>
-
-        @foreach($pastPeriod as $period)
-
-            <h1>{{$period->title}}</h1>
-            <p>started: {{$period->start_date}}</p>
-            <p>ends: {{$period->end_date}}</p>
-
-        @endforeach
-
-    @endif
 @stop
--- resources/views/templates/page.blade.php
+++ resources/views/templates/page.blade.php
@@ -9,20 +9,32 @@
 </head>
 <body>
 
-    <header>
-        @include('partials.nav')
-    </header>
+    <div
+        class="parallax-image-wrapper parallax-image-wrapper-100"
+        data-anchor-target="#dragons + .gap"
+        data-bottom-top="transform:translate3d(0px, 200%, 0px); opacity:2;"
+        data-top-bottom="transform:translate3d(0px, 0%, 0px); opacity:0;"
+    >
 
-    <main>
-        <h1>password123</h1>
+        <div
+            class="parallax-image parallax-image-100"
+            style="background-image:url(img/tesla-2.jpg)"
+            data-anchor-target="#dragons + .gap"
+            data-bottom-top="transform: translate3d(0px, -80%, 0px); "
+            data-top-bottom="transform: translate3d(0px, 80%, 0px); "
+        >
+        </div>
+        <!--the +/-80% translation can be adjusted to control the speed difference of the image-->
+    </div>
+
+    <section id="skrollr-body">
 
         @include('flash::message')
-        @yield('content')
-    </main>
 
-    <footer>
+        @yield('content')
+
         @include('partials.footer')
-    </footer>
+    </section>
 
 </body>
 </html>
