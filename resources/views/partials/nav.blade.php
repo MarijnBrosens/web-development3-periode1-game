eee967a0df02ac94a9df1fd4d33d94f2c44f9e49
MarijnBrosens <marijnbrosens16@hotmail.com>
2015-11-11 00:12:38 +0100

    nav order fix

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,24 +1,20 @@
-<ul class="nav navbar-nav">
+<ul class="nav">
     <li><a href="/" >Home</a></li>
     <li><a href="/winners">Winners</a></li>
     <li><a href="/info">Info</a></li>
 
-
-
     <li><a href="/periods">All periods</a></li>
     <li><a href="/periods/active">Active period</a></li>
     <li><a href="/periods/future">Future periods</a></li>
-</ul>
-<ul>
-    @if(Auth::check())
 
-        <li>nav user-id:  {{Auth::user()->id}}</li>
-        <li><a href="/logout">Logout</a></li>
-        <li><a href="/profile">Welkom {{Auth::user()->firstname}} {{Auth::user()->lastname}}</a></li>
+    @if(Auth::check())
 
         @if(Auth::user()->isAnAdmin())
             <li><a href="/admin/users">users</a></li>
         @endif
+    
+        <li><a href="/profile">Welkom {{Auth::user()->firstname}} {{Auth::user()->lastname}}</a></li>
+        <li><a href="/logout">Logout</a></li>
 
     @else
 
